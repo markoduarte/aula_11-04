419f99a89dd788eb64f8730a1c51e5a9be75b67a
markoduarte <162354537+markoduarte@users.noreply.github.com>
2024-04-12 19:33:45 -0400

    Calculadora com Funções
    Foi implementado as funções para cada operação

--- 0_calculadora.py
+++ 0_calculadora.py
@@ -1,22 +1,28 @@
+def soma(valor1,valor2):
+    return valor1 + valor2
+def subt(valor1,valor2):
+    return valor1 - valor2
+def mult(valor1,valor2):
+    return valor1 * valor2
+def div(valor1,valor2):
+    return valor1 / valor2
+
 valor1 = int(input())
 operador = input()
 valor2 = int(input())
+
 while (valor1 > 0 and valor2 > 0):
     if operador == '+':
-        resultado = (valor1 + valor2)
-        print(resultado)
-        
+        print(soma(valor1,valor2))
+
     elif operador == '-':
-        resultado = (valor1 - valor2)
-        print(resultado)
+        print(subt(valor1,valor2))
                     
     elif operador == '*':
-        resultado = (valor1 * valor2)
-        print(resultado)
+        print(mult(valor1,valor2))
         
     elif operador == '/':
-        resultado = (valor1 / valor2)
-        print(resultado)
+        print(div(valor1,valor2))
                
     else:
         print('operador inválido')
